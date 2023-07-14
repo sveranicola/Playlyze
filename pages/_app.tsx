82616b922190122d08af5c68401b5ba067c8060e
Santiago Vera Nicola <sveranicola12@gmail.com>
2023-07-14 12:10:29 -0400

    added favicon to head in app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel='P icon' href='https://i.imgur.com/E1jfDYI.png' />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
